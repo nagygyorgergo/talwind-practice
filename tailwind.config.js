8ca0b9e39ebc34123a0799ced29333660d926137
nagygyorgergo <nagygyor.gergo@gmail.com>
2024-02-22 15:33:36 +0100

    little modification on homepage, uploaded to new branch

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
     "./src/app/components/**/*.{html,ts}"
   ],
   theme: {
+    container: {
+      /* center: true */
+    },
     extend: {
       colors: {
         transparent: 'transparent',
